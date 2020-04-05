1ac28c15594ad1eeb1bb9620c5d7f9bca54bcb88
salman-ansari-vst-au4 <salmanahmed1497@gmail.com>
2020-04-06 01:27:49 +0530

    connected heroku postgres

--- Database/db-connection.js
+++ Database/db-connection.js
@@ -1,25 +1,32 @@
 const Sequelize = require('sequelize');
-let db, database;
-
-if (process.env.DATABASE_URL) {
-  // the application is executed on Heroku ... use the postgres database
+let db, connection;
+//                           DB Name         username    password                
+// const db = new Sequelize('kisaan_junction', 'postgres', 'postgres', {
+//   host: 'localhost',
+//   dialect: 'postgres'
+// })
+// if (process.env.DATABASE_URL) {
   db = new Sequelize(process.env.DATABASE_URL, {
     dialect: 'postgres',
-    protocol: 'postgres'
-  })
-  database = 'Heroku'
-}
-else {
-  db = new Sequelize('kisaan_junction', 'postgres', 'postgres', {
-    host: 'localhost',
-    dialect: 'postgres'
-  })
-  database = 'Local'
-}
+    protocol: 'postgres',
+    dialectOptions: {
+      ssl: true
+    }
+  });
+//   connection = 'Heroku'
+// }
+// else {
+//   db = new Sequelize('kisaan_junction', 'postgres', 'postgres', {
+//     host: 'localhost',
+//     dialect: 'postgres'
+//   })
+//   connection = 'Local'
+// }
 
 db.authenticate()
   .then(() => {
-    console.log(`Database connected: ${database}`);
+    console.log(`Database connected ${connection}`);
   })
+  .catch((err) => console.error(err))
 
 module.exports = db;
