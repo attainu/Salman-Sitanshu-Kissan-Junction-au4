2833170b8046b8302215d40d9c4d05122cdba146
salman-ansari-vst-au4 <salmanahmed1497@gmail.com>
2020-04-05 18:43:21 +0530

    trying

--- app.js
+++ app.js
@@ -17,5 +17,14 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/users', usersRouter);
 
 // app.use('/table', require('./Controller/CRUD'));
+//Heroku
+if (process.env.NODE_ENV === 'production') {
+  // Serve any static files
+  app.use(express.static(path.join(__dirname, 'client/build')));
+  // Handle React routing, return all requests to React app
+  app.get('*', function (req, res) {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+  });
+}
 
 module.exports = app;
