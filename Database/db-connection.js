8f5d25fa060f58e8a58aded0a3f7df96031b54ca
salman-ansari-vst-au4 <salmanahmed1497@gmail.com>
2020-04-12 23:45:43 +0530

    deploy

--- Database/db-connection.js
+++ Database/db-connection.js
@@ -1,15 +1,16 @@
 const Sequelize = require("sequelize");
 //                           DB Name         username    password
-const db = new Sequelize("agricom", "postgres", "5432", {
-  host: "localhost",
-  dialect: "postgres",
-});
-// const db = new Sequelize(process.env.DATABASE_URL, {
-//   dialect: 'postgres',
-//   protocol: 'postgres',
-//   dialectOptions: {
-//     ssl: true
-//   }
+// const db = new Sequelize("agricom", "postgres", "5432", {
+//   host: "localhost",
+//   dialect: "postgres",
+// });
+const db = new Sequelize(process.env.DATABASE_URL, {
+  dialect: 'postgres',
+  protocol: 'postgres',
+  dialectOptions: {
+    ssl: true
+  }
+})
 
 db.authenticate()
   .then(() => {
