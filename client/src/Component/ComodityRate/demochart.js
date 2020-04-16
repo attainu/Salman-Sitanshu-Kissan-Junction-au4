d263acb3f8039a7cf7723ba63efdf21734fb02da
SITANSHU KUMAR <41771170+sitanshu-kumar-vst-au4@users.noreply.github.com>
2020-04-16 19:03:56 +0530

    comodity rate added

--- client/src/Component/ComodityRate/demochart.js
+++ client/src/Component/ComodityRate/demochart.js
@@ -5,17 +5,31 @@ class Chart extends React.Component {
     labels: ["Min Price", "Max Price", "Modal Price"],
     datasets: [
       {
-        data: [300, 50, 100],
-        backgroundColor: ["#FF6384", "#36A2EB", "#FFCE56"],
-        hoverBackgroundColor: ["#FF6384", "#36A2EB", "#FFCE56"],
+        data: [
+          this.props.item.min_price,
+          this.props.item.max_price,
+          this.props.item.modal_price,
+        ],
+        backgroundColor: ["#28ca2f", "#36A2EB", "#FFCE56"],
+        hoverBackgroundColor: ["#28ca2f", "#36A2EB", "#FFCE56"],
       },
     ],
   };
   render() {
+    console.log();
     return (
       <>
         <div>
-          <h2>Doughnut Example</h2>
+          <h4>
+            {" "}
+            <span style={{ color: "#28ca2f" }}>Commodity Name-</span>
+            {this.props.item.commodity}
+          </h4>
+          <h4>
+            {" "}
+            <span style={{ color: "#28ca2f" }}>Variety Name-</span>
+            {this.props.item.variety}
+          </h4>
           <Doughnut data={this.state} />
         </div>
       </>
