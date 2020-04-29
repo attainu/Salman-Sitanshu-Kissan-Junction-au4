652609ac4aa140de813a1bf4f8704403dea45eb2
salman-ansari-vst-au4 <salmanahmed1497@gmail.com>
2020-04-29 15:55:48 +0530

    deploy

--- Database/db-connection.js
+++ Database/db-connection.js
@@ -15,6 +15,10 @@ const Sequelize = require("sequelize");
 const db = new Sequelize("postgres://tuzxiabdprfdwi:dba837d93cba4c60943ebca1be3bc43274d15bb7ca974aba81898b41892480a6@ec2-52-87-58-157.compute-1.amazonaws.com:5432/d3t6cs0ehomi9l", {
   dialect: 'postgres',
   protocol: 'postgres',
+  logging: false,
+  dialectOptions: {
+    ssl: true
+  }
   // ssl: true,
 })
 
