e3f896c378b1bff5883d43d0e5a4e449c10eb4bd
salman-ansari-vst-au4 <salmanahmed1497@gmail.com>
2020-05-03 17:04:52 +0530

    deploy

--- Database/db-connection.js
+++ Database/db-connection.js
@@ -1,9 +1,9 @@
 const Sequelize = require("sequelize");
 //                           DB Name         username    password
-const db = new Sequelize("agricom", "postgres", "5432", {
-  host: "localhost",
-  dialect: "postgres",
-});
+// const db = new Sequelize("agricom", "postgres", "5432", {
+//   host: "localhost",
+//   dialect: "postgres",
+// });
 // const db = new Sequelize("postgres://tuzxiabdprfdwi:dba837d93cba4c60943ebca1be3bc43274d15bb7ca974aba81898b41892480a6@ec2-52-87-58-157.compute-1.amazonaws.com:5432/d3t6cs0ehomi9l", {
 //   dialect: 'postgres',
 //   protocol: 'postgres',
@@ -12,11 +12,11 @@ const db = new Sequelize("agricom", "postgres", "5432", {
 //   }
 // })
 
-// const db = new Sequelize("postgres://tuzxiabdprfdwi:dba837d93cba4c60943ebca1be3bc43274d15bb7ca974aba81898b41892480a6@ec2-52-87-58-157.compute-1.amazonaws.com:5432/d3t6cs0ehomi9l", {
-//   dialect: 'postgres',
-//   // protocol: 'postgres',
-//   ssl: true,
-// })
+const db = new Sequelize("postgres://tuzxiabdprfdwi:dba837d93cba4c60943ebca1be3bc43274d15bb7ca974aba81898b41892480a6@ec2-52-87-58-157.compute-1.amazonaws.com:5432/d3t6cs0ehomi9l", {
+  dialect: 'postgres',
+  // protocol: 'postgres',
+  ssl: true,
+})
 
 
 db.authenticate()
